555bc48f4d4f3a2778a9f8ab6b2b0b08f0ef8ff3
parksyngjun <darkjyuk@naver.com>
2025-05-23 21:56:26 +0900

    .

--- Chapter1/1-1/test1-1.cpp
+++ Chapter1/1-1/test1-1.cpp
@@ -43,17 +43,38 @@ int main()
 # endif
 
 //문제 3
-// 
+// 숫자를 하나 입력 받아서 그 숫자에 해당하는 구구단을 출력하는 프로그램을 작성
+# if 0
+int main()
+{
+    int num;
+    std::cin >> num;
+
+    for(int i = 1; i<10;i++)
+    {
+        std::cout <<num<< "X" << i << "=" <<num * i<<std::endl;
+    }
+
+}
+# endif
+
+//문제 4
+// 판매원들의 급여 계산 프로그램을 작성.
+// 매달 50만원 급여 & 물품 판매 가격의 12%에 해당하는 돈을 지급.
 # if 1
 int main()
 {
-    char num1[100], num2[100];
-    std::cout << "이름";
-    std::cin >> num1;
-    
-    std::cout << "전화번호";
-    std::cin >> num2;
+    int num;
 
-    std::cout << "이름:"<<num1 <<"전화번호:" <<num2;
+
+    while (1)
+    {
+        std::cout << "판매 금액을 만원 단위로 입력(-1 to end): ";
+        std::cin >> num;
+        if (num==-1) break;
+
+        std::cout << "이번 달 급여: " << 50 + num * 0.12 <<std::endl;
+    }
+
 }
 # endif
