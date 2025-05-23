7129978cfdd3797b23a365513f32e54a8f607606
parksyngjun <darkjyuk@naver.com>
2025-05-23 21:45:27 +0900

    .

---
+++ Chapter1/test1-1.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+//문제 1
+//사용자로부터 총 4개의 정수를 입력 받아서, 그 합을 출력하는 프로그램을 작성
+# if 0
+int main()
+{
+    int num1, num2, num3, num4, num5;
+    std::cout << "1번째 정수 입력:";
+    std::cin >> num1;
+    
+    std::cout << "2번째 정수 입력:";
+    std::cin >> num2;
+
+    std::cout << "3번째 정수 입력:";
+    std::cin >> num3;
+
+    std::cout << "4번째 정수 입력:";
+    std::cin >> num4;
+
+    std::cout << "5번째 정수 입력:";
+    std::cin >> num5;
+
+    int result = num1+num2+num3+num4+num5;
+    std::cout <<"합계:"<<num1+num2+num3+num4+num5;
+}
+# endif
+
+//문제 2
+// 프로그램 사용자로부터 이름과 전화번호를 문자열의 형태로 입력 받아서, 입력 받은 데이터를 그대로 출력하는 프로그램을 작성.
+# if 0
+int main()
+{
+    char num1[100], num2[100];
+    std::cout << "이름";
+    std::cin >> num1;
+    
+    std::cout << "전화번호";
+    std::cin >> num2;
+
+    std::cout << "이름:"<<num1 <<"전화번호:" <<num2;
+}
+# endif
+
+//문제 3
+// 
+# if 1
+int main()
+{
+    char num1[100], num2[100];
+    std::cout << "이름";
+    std::cin >> num1;
+    
+    std::cout << "전화번호";
+    std::cin >> num2;
+
+    std::cout << "이름:"<<num1 <<"전화번호:" <<num2;
+}
+# endif
