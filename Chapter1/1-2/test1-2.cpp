305e979da7f561912d8823ebf11351363aff7774
parksyngjun <darkjyuk@naver.com>
2025-05-26 23:29:05 +0900

    .

--- Chapter1/1-2/test1-2.cpp
+++ Chapter1/1-2/test1-2.cpp
@@ -1,40 +1,40 @@
 #include <iostream>
 
 //문제 1 다음 main 함수에서 필요로 하는 swap 함수를 오버로딩 해서 구현.
-swap(int a, int b)
+void swap(int& a, int& b)
 {
     int tmp = a;
     a = b;
-    b = tmp
+    b = tmp;
     
 }
 
-swap(char a, char b)
+void swap(char& a, char& b)
 {
     char tmp = a;
     a = b;
-    b = tmp
+    b = tmp;
 }
-swap(double a, double b)
+void swap(double& a, double& b)
 {
     double tmp = a;
     a = b;
-    b = tmp
+    b = tmp;
 }
-
+ 
 
 int main()
 {
     int num1 = 20, num2 = 30;
-    swap(&num1, &num2);
+    swap(num1, num2);
     std::cout<<num1<<' '<<num2<<std::endl;
 
     char ch1='A', ch2='Z';
-    swap(&ch1, &ch2);
+    swap(ch1, ch2);
     std::cout<<ch1<<' '<<ch2<<std::endl;
 
     double dbl1 = 1.111, dbl2 = 5.555;
-    swap(&dbl1, &dbl2);
+    swap(dbl1, dbl2);
     std::cout<<dbl1<<' '<<dbl2<<std::endl;
     return 0;
 }
